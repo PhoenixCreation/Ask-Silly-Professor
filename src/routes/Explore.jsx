a0ca516c56e2e71830f0a6a53205a0691c9512d5
PhoenixCreation <phs28052001@gmail.com>
2021-02-24 18:31:49 +0530

    Emergency push.

--- src/routes/Explore.jsx
+++ src/routes/Explore.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
+import Background from '../components/svgs/Background'
 import "../css/routes/Explore.css"
 
 const Explore = () => {
     return (
         <div className="explore">
             <div className="explore__self">
-                this is self part where your questions and answers will be shown
+                <div className="explore__self__cont"></div>
             </div>
             <div className="explore__main">
-                this is the main part where the main questions will be shown and this will be very long list of questions asked in vaious channels.
+                <div className="explore__main__cont"></div>
             </div>
             <div className="explore__suggested">
-                this is the part where we suggest the different channels for this.
+                <div className="explore__suggested__cont"></div>
             </div>
         </div>
     )
